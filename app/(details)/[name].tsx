15585f21e8fbb6fddc321afdbfa9870d834e1699
hikmet <aydin96tr@gmail.com>
2025-01-05 06:24:18 +0300

    style details page

--- app/(details)/[name].tsx
+++ app/(details)/[name].tsx
@@ -1,4 +1,4 @@
-import { Link, useLocalSearchParams } from "expo-router";
+import { useLocalSearchParams } from "expo-router";
 import { useVideoPlayer, VideoView } from "expo-video";
 import { SafeAreaView, Text } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
@@ -13,14 +13,10 @@ export default function Details() {
   return (
     <SafeAreaProvider>
       <SafeAreaView className="flex-1">
-        <Text>Test</Text>
-        <Text>Clip details</Text>
-        <Text>{name}</Text>
+        <Text className="text-xl">Clip details</Text>
+        <Text>Clip name: {name}</Text>
         <VideoView style={{ width: 350, height: 275 }} player={player} allowsFullscreen allowsPictureInPicture />
-        <Text>{description}</Text>
-        <Link href={"/"} className="bg-blue-300">
-          Go back
-        </Link>
+        <Text>Clip description: {description}</Text>
       </SafeAreaView>
     </SafeAreaProvider>
   );
