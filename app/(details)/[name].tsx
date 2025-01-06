15cd5c4737a2ab634a700115de65ef8ca6f28324
hikmet <aydin96tr@gmail.com>
2025-01-06 05:39:43 +0300

    add some styles

--- app/(details)/[name].tsx
+++ app/(details)/[name].tsx
@@ -1,6 +1,6 @@
 import { VideoPlayerComponent } from "@/components/video-player";
 import { useLocalSearchParams } from "expo-router";
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView, Text, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function Details() {
@@ -8,11 +8,13 @@ export default function Details() {
 
   return (
     <SafeAreaProvider>
-      <SafeAreaView className="flex-1">
-        <Text className="text-xl">Clip details</Text>
-        <Text>Clip name: {name}</Text>
-        <VideoPlayerComponent uri={uri as string} />
-        <Text>Clip description: {description}</Text>
+      <SafeAreaView className="m-2 flex-1">
+        <Text className="mb-2 text-xl">Clip details</Text>
+        <Text className="mb-2">Clip name: {name}</Text>
+        <View style={{ alignItems: "center" }}>
+          <VideoPlayerComponent uri={uri as string} />
+        </View>
+        <Text className="mt-2">Clip description: {description}</Text>
       </SafeAreaView>
     </SafeAreaProvider>
   );
